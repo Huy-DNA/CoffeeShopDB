45ddf01749c622164fc0edaf93b4cccb53b48f71
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-04-19 21:02:09 +0700

    Basic dish list display

--- routes/dishes.js
+++ routes/dishes.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 var model = require('../model/model');
 
-router.get('/', function (req, res, next) {
-     
+router.get('/', async function (req, res, next) {
+    const tmp = await model.from(['Dish D', 'DishGroup G'])
+                           .whereAttribute('D.group_id', '=', 'G.id')
+                           .select(['D.name', 'G.name', 'D.url'])
+                           .query()
+    const dishList = tmp.map(e => ({
+        name: e.name[0],
+        group: e.name[1],
+        url: e.url,
+    }))
+    res.render('dishes', { dishList })
 })
 
 router.get('/add', function (req, res, next) {
