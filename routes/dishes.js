c472c9cfe07f07913f0a96f97667d67b7084019d
unknown <xuantho573@gmail.com>
2023-04-23 16:43:16 +0700

    Create form to add new dish

--- routes/dishes.js
+++ routes/dishes.js
@@ -1,21 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var model = require('../model/model');
+var model = require("../model/model");
 
-router.get('/', function (req, res, next) {
-     
-})
+router.get("/", function (req, res, next) {});
 
-router.get('/add', function (req, res, next) {
+router.get("/add", async function (req, res, next) {
+    const tmp = await model.from(["DishGroup"]).select(["name", "id"]).query();
+    const group_dishes = tmp.map((e) => ({
+        name: e.name,
+        id: e.id,
+    }));
 
-})
+    res.render("dishform", { group_dishes });
+});
 
-router.post('/add', function (res, req, next) {
+router.post("/add", async function (req, res, next) {
+    res.redirect("/dishes");
+    var {
+        id,
+        name,
+        url,
+        group_id,
+        dtype,
+        fprice,
+        fstart,
+        fend,
+        dsize,
+        dprice,
+    } = req.body;
+    console.log(req.body);
+});
 
-})
-
-router.get('/:dishId', function (req, res, next) {
-    
-})
+router.get("/:dishId", function (req, res, next) {});
 
 module.exports = router;
